97a473d87176c250bc2d7700c1bbbfe99b5dc543
Louistam888 <louistam888@gmail.com>
2025-07-24 20:23:20 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+var age int = 29
+var name = "syntax fuel"
+
+var isOnline bool = true
+var price float64 = 99.99
+
+const Pi = 3.1415
+
+// func greet(name string) {
+// 	fmt.Println("hello,", name)
+// }
+
+// func add(a int, b int) int {
+// 	return a + b
+// }
+
+// func divide(a, b float64) (float64, error) {
+// 	if b == 0 {
+// 		return 0, fmt.Errorf("cannot divide by zero")
+// 	}
+// 	return a / b, nil
+// }
+
+// func main() {
+// 	fmt.Println(add(1, 2))
+// 	fmt.Println(divide(2, 4))
+// }
+
+// func main() {
+// 	score := 85
+// 	if score >= 90 {
+// 		fmt.Println("Excellent")
+// 	} else if score >= 75 {
+// 		fmt.Println("Good")
+// 	} else {
+// 		fmt.Println("kKeep Practicing")
+// 	}
+// }
+
+fun
